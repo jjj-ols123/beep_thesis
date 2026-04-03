65812809ec750ff44f8642750bea6f96f4f974a9
John Code <fuentes.289565@sjdelmonte.sti.edu.ph>
2026-04-03 23:38:46 +0800

    header changes

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,36 +1,26 @@
-import logo from '../assets/Untitled.png';
-import { useState } from 'react';
+import logo from '../assets/train-logo.png';
 import LoginForm from './loginform';
 
 
 function Header() { 
 
-    const [showLogin, setShowLogin] = useState(false);
-
-    function handleLoginClick() { 
-        setShowLogin(true);
-    }
-
     return (
         <>
         <nav className="header"> 
             <div className='left_nav'>
-                <img src={logo} alt="STI Logo" className="logo_image" />
+                <img src={logo} alt="Train Logo" className="logo_image" />
                 <h1 className="brand"> 
-                    STI Education Services Group
+                    
                 </h1>    
             </div>
 
             <div className='right_nav'>
             <ul className='nav-links'>
-                <li><a href="" className="header_text">Campus Helpdesk</a></li>
+                <li><a href="" className="header_text">Services</a></li>
+                <li><a href="" className="header_text">About Us</a></li>
+                <li><a href="" className="header_text">Contacts</a></li>
                 <li><a href="" className="header_text">FAQ</a></li>
             </ul>
-            <button className='login-button' onClick={handleLoginClick}>Log in</button>
-
-            {
-                showLogin && <LoginForm />
-            }
             </div>
         </nav>
         </>
